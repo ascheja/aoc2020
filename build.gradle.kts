bded32a2ccf4948b9f246f1059d0106a2f39eb0a
Andreas Scheja <asc@sinceat.net>
2020-12-10 00:10:17 +0100

    Add gradle task for adding new subprojects

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import java.nio.file.Files
+import java.nio.file.StandardOpenOption
+
 plugins {
     kotlin("jvm") version "1.4.20"
 }
@@ -18,4 +21,26 @@ subprojects {
 
 dependencies {
     implementation(kotlin("stdlib"))
+}
+
+task("newDay") {
+    doLast {
+        val newDay = "day" + (subprojects.map { it.name.removePrefix("day").toInt() }.max()!! + 1)
+        val rootPath = rootDir.toPath()
+        val kotlinPath = rootPath.resolve("$newDay/src/main/kotlin")
+        Files.createDirectories(kotlinPath)
+        Files.write(
+            kotlinPath.resolve("App.kt"),
+            listOf(
+                "package net.sinceat.aoc2020.$newDay",
+                "",
+                "fun main() {",
+                """    println("Hello World!")""",
+                "}",
+                ""
+            ),
+            StandardOpenOption.CREATE
+        )
+        Files.createDirectories(rootPath.resolve("$newDay/src/main/resources"))
+    }
 }
